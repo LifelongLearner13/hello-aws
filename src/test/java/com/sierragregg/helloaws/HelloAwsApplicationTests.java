ff7126e1b846d4099c71ce5d59f228d58c64ac2b
LifelongLearner13 <sierragregg@gmail.com>
2020-05-23 08:32:27 -0500

    Initial commit

---
+++ src/test/java/com/sierragregg/helloaws/HelloAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sierragregg.helloaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
